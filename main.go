bbca7cb557b70c62f2e72a4a2705f311c6c63e20
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2022-01-10 22:19:38 +0530

    Small fixes

--- main.go
+++ main.go
@@ -13,14 +13,12 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(os.Getenv("AA_DIR_WEBSITE"))))
 
-	http.HandleFunc("/download/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/storage/download/", func(w http.ResponseWriter, r *http.Request) {
 		file_path, err := url.PathUnescape(r.URL.Path[len("/download/"):])
 		if err != nil {
-			fmt.Println("Unable to open file")
+			fmt.Println("Unable to unescape path")
 		}
 		file_path = path.Join(os.Getenv("AA_DIR_STORAGE"), file_path)
-		fmt.Println(r.URL.Path)
-		fmt.Println(file_path)
 
 		file, err1 := os.Open(file_path)
 		if err1 != nil {
@@ -37,7 +35,7 @@ func main() {
 		w.Header().Set("Content-Disposition", "attachment; filename="+path.Base(file_path))
 
 		io.Copy(w, file)
-		fmt.Println("File sent")
+        fmt.Println("File sent: "+file_path)
 	})
 
 	http.ListenAndServe(":8080", nil)
